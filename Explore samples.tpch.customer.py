e6fe0a05d4c5e602e5b933677105fe69c3239543
ganeshnbs07 <ganeshnbs07>
2023-06-05 00:13:47 +0000

    Firstfile

---
+++ Explore samples.tpch.customer.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC SELECT * FROM `samples`.`tpch`.`customer`;
+
+# COMMAND ----------
+
+# MAGIC
+# MAGIC
+# MAGIC %sql
+# MAGIC SELECT c_name, c_phone FROM `samples`.`tpch`.`customer`
+# MAGIC where c_mktsegment = 'BUILDING'
+# MAGIC order by c_name;
